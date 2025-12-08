41df97cccd530a80e29c00beca445389ae1037fc
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-12-08 22:17:25 +0600

    api for get, update and delete users from usersCollection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 
@@ -47,6 +47,45 @@ async function run() {
         const result = await usersCollection.insertOne(userInfo);
         res.send(result)
     }) 
+
+    // get all users 
+    app.get('/users',  async(req,res)=>{
+    const searchText = req.query.searchText;
+    const filter = req.query.filter
+    const query = {};
+    if(searchText){
+      query.displayName = {$regex: searchText, $options:"i"}
+    }
+    if(filter){
+        query.role = filter 
+    }
+    const cursor = usersCollection.find(query);
+    const result = await cursor.toArray()
+    res.send(result)
+   })
+
+   // update user role
+   app.patch('/users/:id/role', async(req,res)=>{
+    const id = req.params.id;
+    const roleInfo = req.body
+    console.log(roleInfo)
+    const query = {_id: new ObjectId(id)}
+    const updatedDoc = {
+      $set:{
+        role:roleInfo.role
+      }
+    }
+    const result = await usersCollection.updateOne(query, updatedDoc)
+    res.send(result)
+
+   })
+//     delete user
+   app.delete('/users/:id', async(req, res)=>{
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await usersCollection.deleteOne(query)
+    res.send(result)
+   })
     
      
     await client.db("admin").command({ ping: 1 });

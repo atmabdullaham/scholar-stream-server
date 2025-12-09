acfa7588b6080b52645195c2f5dbd7191ccec730
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-12-09 23:42:21 +0600

    added api to save scholarship in mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const ImageKit = require('@imagekit/nodejs');   
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -12,6 +13,32 @@ app.use(
   cors()
 );
 app.use(express.json())
+
+
+const imgkitClient = new ImageKit(
+    {
+    publicKey : "public_9ZiMp/MhVuE8NR/DGLRM9LRbiV0=",
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : "https://ik.imagekit.io/atm"  
+});
+
+// allow cross-origin requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/auth', function (req, res) {
+  // Your application logic to authenticate the user
+  // For example, you can check if the user is logged in or has the necessary permissions
+  // If the user is not authenticated, you can return an error response
+  const { token, expire, signature } = imgkitClient.helper.getAuthenticationParameters();
+  res.send({ token, expire, signature, publicKey: process.env.IMAGEKIT_PUBLIC_KEY });
+});
+
 
 
 const uri = `mongodb+srv://${process.env.db_admin}:${process.env.db_pass}@cluster0.a41jis3.mongodb.net/?appName=Cluster0`;
@@ -32,9 +59,11 @@ async function run() {
       await client.connect();
      const database = client.db('scholar_stream');
      const usersCollection  = database.collection('users');
+     const scholarshipsCollection  = database.collection('scholarships');
      
-
-    //  users related apis
+      //$  users related apis
+     // _________________________________________________________________________
+    // 1. Save user
     app.post("/users", async(req, res)=>{
         const userInfo = req.body
         userInfo.role = 'student';
@@ -47,8 +76,7 @@ async function run() {
         const result = await usersCollection.insertOne(userInfo);
         res.send(result)
     }) 
-
-    // get all users 
+    // 2. get all users 
     app.get('/users',  async(req,res)=>{
     const searchText = req.query.searchText;
     const filter = req.query.filter
@@ -63,8 +91,7 @@ async function run() {
     const result = await cursor.toArray()
     res.send(result)
    })
-
-   // update user role
+   // 3. update user role
    app.patch('/users/:id/role', async(req,res)=>{
     const id = req.params.id;
     const roleInfo = req.body
@@ -79,13 +106,24 @@ async function run() {
     res.send(result)
 
    })
-//     delete user
+   // 4. delete user
    app.delete('/users/:id', async(req, res)=>{
     const id = req.params.id
     const query = {_id: new ObjectId(id)}
     const result = await usersCollection.deleteOne(query)
     res.send(result)
    })
+
+      //$ scholarships related apis
+     //__________________________________________________
+    // 1.  Save scholarship
+    app.post('/scholarships', async(req,res)=>{
+        const scholarshipInfo = req.body
+        scholarshipInfo.scholarshipPostDate = new Date()
+        const result = await scholarshipsCollection.insertOne(scholarshipInfo)
+        res.send(result)
+    })
+
     
      
     await client.db("admin").command({ ping: 1 });
